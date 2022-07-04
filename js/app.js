8599b22d96945419d47a4bcd4125a3dea92d8513
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-07-04 16:16:10 -0600

    main functionality and styling for iphone lock sreen

--- js/app.js
+++ js/app.js
@@ -1,2 +1,114 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const page = document.querySelector('main');
+const keyboard = document.createElement('div');
+const title = document.createElement('h1');
+const codeDisplay = document.createElement('h2');
+const enteredCodeDisplay = document.createElement('div');
+const timeoutInterval = 1000 * 1;
+const keyboardAlphabet = {
+  2: 'abc',
+  3: 'def',
+  4: 'ghi',
+  5: 'jkl',
+  6: 'mno',
+  7: 'pqrs',
+  8: 'tuv',
+  9: 'wxyz',
+};
+
+let code = createFourDigitNumber();
+let enteredCode = '';
+
+title.innerText = 'Enter Passcode';
+page.append(title);
+createCirclesForentredCodeDisplay();
+enteredCodeDisplay.classList.add('entered-code-display');
+page.append(enteredCodeDisplay);
+codeDisplay.innerText = `Code : ${code}`;
+page.append(codeDisplay);
+
+creatInputButtonsToKeyboard();
+
+keyboard.classList.add('keyboard');
+page.append(keyboard);
+
+function checkPassCode(number) {
+  const codeDisplay = document.querySelector('h2:nth-child(3)');
+  const buttons = document.querySelectorAll('button');
+  enteredCode += number;
+  if (enteredCode.length === 4 && enteredCode === code) {
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+    fillEnteredCodeDisplay(enteredCode.length);
+    code = createFourDigitNumber();
+    enteredCode = '';
+    setTimeout(() => {
+      fillEnteredCodeDisplay(enteredCode.length);
+    }, 400);
+    setTimeout(() => {
+      buttons.forEach((button) => {
+        button.disabled = false;
+      });
+      codeDisplay.innerText = `Code: ${code}`;
+    }, 700);
+    return;
+  }
+  if (enteredCode.length === 4) {
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+    enteredCodeDisplay.classList.add('error');
+    fillEnteredCodeDisplay(enteredCode.length);
+    enteredCode = '';
+    setTimeout(() => {
+      buttons.forEach((button) => {
+        button.disabled = false;
+      });
+      fillEnteredCodeDisplay(enteredCode.length);
+      enteredCodeDisplay.classList.remove('error');
+    }, timeoutInterval);
+    return;
+  }
+  fillEnteredCodeDisplay(enteredCode.length);
+}
+
+function creatInputButtonsToKeyboard() {
+  for (let i = 0; i < 10; i++) {
+    const button = document.createElement('button');
+    button.innerHTML = `<span>${i}</span> <span>${
+      keyboardAlphabet[i] ? keyboardAlphabet[i] : ''
+    }</span>`;
+    button.addEventListener('click', () => {
+      checkPassCode(i);
+    });
+    keyboard.append(button);
+  }
+}
+
+function createFourDigitNumber() {
+  let number = '';
+  for (let i = 0; i < 4; i++) {
+    number += `${Math.floor(Math.random() * 10)}`;
+  }
+  return number;
+}
+
+function createCirclesForentredCodeDisplay() {
+  for (let i = 0; i < 4; i++) {
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    enteredCodeDisplay.append(circle);
+  }
+}
+
+function fillEnteredCodeDisplay(codeLength) {
+  const circles = document.querySelectorAll('.circle');
+  circles.forEach((circle) => {
+    circle.classList.remove('fill');
+  });
+  circles.forEach((circle, index) => {
+    if (index < codeLength) {
+      circle.classList.add('fill');
+    }
+  });
+}
